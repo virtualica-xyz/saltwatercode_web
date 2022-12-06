987bf9c99017360f49774f3f60503caffe4ae364
Piotr <109795776+knopersk@users.noreply.github.com>
2022-12-06 09:26:07 +0100

    Update Services.tsx

--- components/Services.tsx
+++ components/Services.tsx
@@ -9,14 +9,14 @@ const Services = () => {
           "px-4 sm:px-20 bg-hero bg-no-repeat bg-cover bg-center bg-fixed mb-8 sm:mb-24  mt-0 sm:mt-16 "
         }
       >
-        <div className="container mx-auto text-white font-press-start flex  h-full  place-content-center flex-col content-center text-left gap-2">
-          <h2 className="container mx-auto font-press-start flex text-center h-full  place-content-center flex-col content-center text-left gap-2 text-transparent  bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
+        <div className="container mx-auto text-white font-nunito flex  h-full  place-content-center flex-col content-center text-left gap-2">
+          <h2 className="container mx-auto font-nunito flex text-center h-full  place-content-center flex-col content-center text-left gap-2 text-transparent  bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
             Our services :
           </h2>
         </div>
       </div>
 
-      <div className="container px-4 sm:px-0 mx-auto text-white font-press-start flex  place-content-center gap-8 sm:gap-20 flex-wrap">
+      <div className="container px-4 sm:px-0 mx-auto text-white font-nunito flex  place-content-center gap-8 sm:gap-20 flex-wrap">
         <div className="w-full transition ease-in-out duration-300 hover:scale-100 sm:hover:scale-110 max-w-sm p-4 bg-black text-white border rounded-lg shadow-md sm:p-8 dark:bg-gray-800 dark:border-gray-700">
           <div className="flex items-baseline place-content-center text-center text-gray-50 dark:text-white">
             <span className="text-lg mt-2 sm:mt-0 sm:text-3xl text-center  font-extrabold tracking-tight">
