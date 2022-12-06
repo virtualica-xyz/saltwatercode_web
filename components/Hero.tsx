f206ea22b735b8ecf1e0043ccc77aef7cf41c29b
Piotr <109795776+knopersk@users.noreply.github.com>
2022-12-06 09:20:07 +0100

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -8,8 +8,8 @@ const Hero = () => {
         " h-100 sm:h-128  px-4 sm:px-20 bg-hero  bg-no-repeat bg-cover bg-center bg-fixed mb-0 sm:mb-24 mt-4 sm:mt-16 sm:bg-hero-none sm:bg-hero-img  "
       }
     >
-      <div className="container mx-auto text-white font-nunito flex  h-full  place-content-center flex-col content-center text-left gap-2">
-        <h1 className="text-white text-lg sm:text-4xl font-nunito text-transparent  bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
+      <div className="container mx-auto text-white font-press-start flex  h-full  place-content-center flex-col content-center text-left gap-2">
+        <h1 className="text-white text-lg sm:text-4xl font-press-start text-transparent  bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
           Digital Supply Chain Management
         </h1>
         <div className={"text-sm sm:text-base"}>
