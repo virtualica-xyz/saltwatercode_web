a284740d44644549cfe5fa3bacf1d7e02f248d0f
Piotr <109795776+knopersk@users.noreply.github.com>
2022-12-07 09:25:27 +0100

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -10,10 +10,11 @@ const Hero = () => {
     >
       <div className="container mx-auto text-white font-press-start flex  h-full  place-content-center flex-col content-center text-left gap-2">
         <h1 className="text-white text-lg sm:text-4xl font-press-start text-transparent  bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
-          Digital Supply Chain Management
+          A Web2.5 consultancy & software
         </h1>
         <div className={"text-sm sm:text-base"}>
-          <p>Blockchain-powered technology</p>
+          <p>blockchain-powered technology</p>
+          <p>human experience</p>
         </div>
       </div>
     </div>
