849673a2afd70f0949eb952a3f36dfa1b0955860
Piotr <109795776+knopersk@users.noreply.github.com>
2022-12-08 12:33:46 +0100

    Update Contact.tsx

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -3,8 +3,19 @@ import React from "react";
 const Contact = () => {
   return (
     <div id={"Contact"}>
-      <div data-tf-widget="ATm6wuGW" data-tf-opacity="100" data-tf-iframe-props="title=test" style="width:100%;height:100%;"></div>
-      <script src="//embed.typeform.com/next/embed.js"></script>
+      <div className="container mx-auto font-press-start h-72 flex  content-center  flex-col place-items-center place-content-center mt-6">
+        <h2 className="mb-4 text-lg sm:text-2xl text-black tracking-tight font-bold ">
+          Connect with us
+        </h2>
+        <h2 className={"text-black text-lg sm:text-xl font-press-start "}>
+          <a
+            className={"no-underline hover:underline"}
+            href="mailto: abc@example.com"
+          >
+            abc@example.com
+          </a>
+        </h2>
+      </div>
     </div>
   );
 };
